ef321a501f56ebb1ec984bebbf0bad664797e5c3
andikapp98 <andikapp1998@gmail.com>
2023-06-04 11:39:39 +0700

    typedSpeed

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ MenuBtn.addEventListener('click', function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+let typed = new Typed('.auto-input',{
+    strings: ['Programmer!', 'Fotographer!', 'Freelancer!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})

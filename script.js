39fb50f5bde74af23d62cdf2f8d0b05bcfbd3ac1
andikapp98 <andikapp1998@gmail.com>
2023-06-26 21:43:48 +0700

    end

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ let typed = new Typed('.auto-input',{
     backDelay: 2000,
     loop: true,
 })
+
+
+// Active Scroll
+
+let navLinks = document.querySelectorAll('nav ul li a')
+
+let sections = document.querySelectorAll('section');
+console.log(sections)
+
+window.addEventListener('scroll', function (){
+    const scrollPos = window.scrollY
+    sections.forEach(section =>{
+        if(scrollPos> section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active')
+                if (section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+
+        }
+    });
+});
